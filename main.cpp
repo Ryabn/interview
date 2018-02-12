7e72e94a3b61fa794bf5601ea223e2f823056e8d
Ryan Yang <javathon@gmail.com>
2018-02-11 16:04:26 -0800

    first commit

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+
+void givenPointFindPaths(int x,int y){
+    if(x != 0 && field[x+1][y] != 0){
+
+    }
+}
+int field[4][5] = {{1,1,0,0,0},
+                   {0,1,0,0,0},
+                   {1,1,9,0,1},
+                   {1,0,1,0,1}};
+
+int main(){
+
+
+
+
+    return 0;
+}
+
